e5854c46c41ca2e17d4a6c4deff6b9febaa972ab
Your Name <you@example.com>
2026-04-19 23:17:32 +0530

    updated project pdf

--- Analytics/Project1/main.py
+++ Analytics/Project1/main.py
@@ -1,10 +1,15 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+#load CSV file
 df=pd.read_csv('railway_gauges 1.csv')
 df.head()
+
+
+#Find which year has the maximum installation
 df.iloc[[df['Total'].idxmax()]]
 
+#plot Data using bar chart
 df=df.drop('Total',axis=1)
 ax=df.plot(x="Year",kind="bar")
 plt.xticks(rotation=70)
